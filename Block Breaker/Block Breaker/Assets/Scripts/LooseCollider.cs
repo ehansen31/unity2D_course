26e3b8bb0101c0d2e83f8af6606b25ee37e96313
Erik <e.hansen31@live.com>
2020-04-23 22:54:58 -0400

    progress on brick breaker

--- Block Breaker/Block Breaker/Assets/Scripts/LooseCollider.cs
+++ Block Breaker/Block Breaker/Assets/Scripts/LooseCollider.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LooseCollider : MonoBehaviour
 {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        SceneManager.LoadScene("Win Screen");
     }
 }
---
+++ Block Breaker/Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float ScreenWidthInUnits = 16f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xPos = Input.mousePosition.x / Screen.width * ScreenWidthInUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(xPos, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
