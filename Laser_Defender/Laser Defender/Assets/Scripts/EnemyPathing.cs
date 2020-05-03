0c48fa58db40a88a0b9092f83b49da384a4b1f45
Erik <e.hansen31@live.com>
2020-05-02 23:06:06 -0400

    stop on making enemies fire

--- Laser_Defender/Laser Defender/Assets/Scripts/EnemyPathing.cs
+++ Laser_Defender/Laser Defender/Assets/Scripts/EnemyPathing.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    [SerializeField] List<Transform> waypoints;
-    [SerializeField] float moveSpeed = 2f;
+    WaveConfig waveConfig;
+    List<Transform> waypoints;
     int waypointIndex = 0;
 
-    // Start is called before the first frame update
     void Start()
     {
+        waypoints = waveConfig.GetWaypoints();
         transform.position = waypoints[waypointIndex].transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
         Move();
+    }
+
+    public void SetWaveConfig(WaveConfig waveConfig)
+    {
+        this.waveConfig = waveConfig;
     }
 
     private void Move()
@@ -25,7 +29,7 @@ public class EnemyPathing : MonoBehaviour
         if (waypointIndex <= waypoints.Count - 1)
         {
             var targetPosition = waypoints[waypointIndex].transform.position;
-            var movementThisFrame = moveSpeed * Time.deltaTime;
+            var movementThisFrame = waveConfig.GetMoveSpeed() * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
             if (transform.position == targetPosition)
             {
---
+++ Laser_Defender/Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+    [SerializeField] bool looping = false;
+    IEnumerator Start()
+    {
+        do
+        {
+            yield return StartCoroutine(SpawnAllWaves());
+        } while (looping);
+    }
+
+    private IEnumerator SpawnAllWaves()
+    {
+        foreach (var waveConfig in waveConfigs)
+        {
+            yield return SpawnAllEnemiesInWave(waveConfig);
+        }
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig currentWave)
+    {
+        for (int i = 0; i < currentWave.GetNumberOfEnemies(); i++)
+        {
+            var newEnemy = Instantiate(
+            currentWave.GetEnemyPrefab(),
+            currentWave.GetWaypoints()[0].transform.position,
+            Quaternion.identity);
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(currentWave);
+            yield return new WaitForSeconds(currentWave.GetTimeBetweenSpawns());
+        }
+    }
+}
